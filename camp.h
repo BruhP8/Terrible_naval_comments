421c2f5f500f3876d465de14da0b2da32fab99d8
TheTitoPlay <vocatis.stark@gmail.com>
2019-02-14 20:16:40 +0100

    Commentaires pour le camp

--- camp.h
+++ camp.h
@@ -1,13 +1,21 @@
 #ifndef CAMP_H
 #define CAMP_H
+
 #include "player.h"
 #include "darray.h"
+
 typedef struct camp_s {
 	darray_t *players;
 	int is_alive;
 	char *team_string;
 } camp_t;
+
+/// Création d'un camp
 camp_t *newCamp();
+
+///
 char *campTeamString(camp_t *);
+
+/// Supprime le camp donné et libère la mémoire utilisée
 void deleteCamp(camp_t *);
-#endif 
+#endif
