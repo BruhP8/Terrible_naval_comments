34e8b3b2e63f72f9116c16411e8085f6e5124fd5
TheTitoPlay <vocatis.stark@gmail.com>
2019-02-14 19:52:54 +0100

    Commentaires un peu bofs mais ça passe

--- game_state.h
+++ game_state.h
@@ -1,10 +1,12 @@
 #ifndef GAME_STATE_H
 #define GAME_STATE_H
+
 #include "cell.h"
 #include "result.h"
 #include "camp.h"
 #include "darray.h"
 #include "camp_allocator.h"
+
 typedef struct game_state_s {
 	cell_t *grid;
 	int width;
@@ -15,11 +17,24 @@ typedef struct game_state_s {
 	darray_t *camps;
 	camp_t *winning;
 } game_state_t;
+
+/// Défini l'espace de jeu de la nouvelle partie
 game_state_t *newGame();
+
+/// Retourne les informations sur le tir effectué
 result_t doAction(game_state_t *, player_t *, point_t);
+
+/// Vérifie s'il reste toujours deux joueur en jeu
 int turnEndUpdate(game_state_t *);
+
+/// Vérifie si le point donné appartient au rectangle donné
 int isPointInsideRect(point_t p, point_t rect[2]);
+
 void interruptHandler(int s);
+
+/// Retourne la case correspondant au point donné
 cell_t *getCell(game_state_t *game, point_t co);
+
+/// Retourne les coordonnées d'une case donné
 point_t getCoordinates(game_state_t *game, cell_t *c);
-#endif 
+#endif
